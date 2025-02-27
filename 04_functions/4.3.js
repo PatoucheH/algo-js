8b51d5edd02197c4f11f631e6aa2c960441aa7ca
Hugo Pat <Hugo.Paternoster@hotmail.com>
2025-02-27 19:56:04 +0100

    Ad JSDOC 4.3.js

--- 04_functions/4.3.js
+++ 04_functions/4.3.js
@@ -5,7 +5,12 @@ const prompt = promptSync();
 
 let n = prompt("Enter the size of the array that you want : ");
 let arrayNumber = [];
-
+/**
+ *
+ * @param {number} n -Size of the array
+ * @returns The arrays with n number of random number
+ * Create an array of n number of random number
+ */
 function multiRand(n) {
   for (let i = 0; i < n; i++) {
     let numToAdd = rand10();
