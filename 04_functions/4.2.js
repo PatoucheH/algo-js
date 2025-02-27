515f74515eb314c0cd3de3693036dc12bf9e8b7a
Hugo Pat <Hugo.Paternoster@hotmail.com>
2025-02-27 17:22:06 +0100

    add 4.2.js

---
+++ 04_functions/4.2.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @param {number} min -Minimum
+ * @param {number} max -Maximum
+ * @returns -Return a number between 1 and 10
+ * Generates a number between 0 and 1 multiplicate it by 10 then round up
+ */
+
+function rand10(min, max) {
+  let num = Math.floor(Math.random() * 10);
+
+  return num;
+}
+
+console.log(rand10());
