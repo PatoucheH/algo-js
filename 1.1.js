d6000a36a0725701ca8d8c45b1ebd562c8abe579
Hugo Pat <Hugo.Paternoster@hotmail.com>
2025-02-27 10:41:48 +0100

    first commit

---
+++ 1.1.js
@@ -0,0 +1,3 @@
+let age = 25;
+let nom = "Hugo";
+console.log("Bonjour je m'appelle " + nom + " et j'ai " + age);
---
+++ 1.2.js
@@ -0,0 +1,4 @@
+let name = "Dutronc"
+let firstname = "Jacques"
+let city = "Bruxelles"
+console.log("Your name is " + firstname + " " + name + " and you live in " + city +".")
