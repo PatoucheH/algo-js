92c744c3aa65eed1950facc25bfccc9000578a0a
Hugo Pat <Hugo.Paternoster@hotmail.com>
2025-03-03 14:49:12 +0100

    correction 5.2.js

--- 05_objects/5.1.js
+++ 05_objects/5.1.js
@@ -1,18 +1,30 @@
 import promptSync from "prompt-sync";
 
 const prompt = promptSync();
-
+/**
+ * ask to the user to enter the name, the year or production and the members of his favorite Tv shows
+ * @returns an object with these informations
+ */
 export function askTvSerie() {
   let name = prompt("Enter the name of your favorite TV serie : ");
   let productionYear;
+  let cast = [];
+  let input;
   while (isNaN(productionYear)) {
     productionYear = prompt(
       "Enter the year of your serie begin to be product : "
     );
   }
-  let cast = prompt("Enter the name of the cast member in your TV serie : ")
-    .replace(/[\[\]]/g, "")
-    .split(/[,.]/);
+
+  do {
+    input = prompt(
+      "Enter the name of the cast of your TV show (Add one name or more separate by ',' ) exit the program by pressing enter an empty entry : "
+    );
+
+    cast
+      .push(...input.replace(/[\[\]]/g, "").split(/[,.]/)
+      .filter((name) => name.length > 0));
+  } while (input !== "");
   //   console.log(name, productionYear, cast);
 
   const tvSerie = {
@@ -26,7 +38,6 @@ export function askTvSerie() {
 // console.log(askTvSerie())
 
 let tvSerie = askTvSerie();
-
 
 export let jsonTvSerie = JSON.stringify(tvSerie);
 
--- 05_objects/5.2.js
+++ 05_objects/5.2.js
@@ -1,5 +1,9 @@
 import { jsonTvSerie } from "./5.1.js";
-
+/**
+ * 
+ * @param {object} tvSerie object with the information about the favorite tv show of the users
+ * @returns  the list of the cast members reversed
+ */
 export function randomizeCast(tvSerie) {
   let tvSerieObj = JSON.parse(jsonTvSerie);
   let randListOfCast = tvSerieObj.casting.slice().reverse();
